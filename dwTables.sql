358b133fbf63c53dee058a7bd88f6a5c92089520
Georgios Antoniadis <62813754+georgios-antoniadis@users.noreply.github.com>
2022-01-11 22:23:32 +0200

    Add files via upload

---
+++ dwTables.sql
@@ -0,0 +1,110 @@
+-- CREATE Schema IF NOT EXISTS dw;
+DROP TABLE if exists dw.date;
+DROP TABLE if exists dw.reaction;
+DROP TABLE if exists dw.animal;
+DROP TABLE if exists dw.incident;
+DROP TABLE if exists dw.outcome;
+DROP TABLE if exists dw.drug;
+DROP TABLE if exists dw.fact;
+
+CREATE TABLE dw.reaction(
+    reaction_id                 VARCHAR(128),
+    veddra_version              VARCHAR(128),
+    veddra_term_code            VARCHAR(128),
+    number_of_animals_affected  VARCHAR(128),
+    veddra_term_name            VARCHAR(128)
+);
+
+CREATE TABLE dw.animal(
+    animal_id                           VARCHAR(128),
+    species 	                        VARCHAR(128),
+    gender 	                            VARCHAR(128),
+    female_animal_physiological_status 	VARCHAR(128),
+    age                                 DECIMAL(21,6),
+    age_unit 	                        VARCHAR(128),
+    "weight_kg"    	                    DECIMAL(21,6),
+    is_crossbred 	                    VARCHAR(128),
+    breed_component 	                VARCHAR(128),
+    reproductive_status 	            VARCHAR(128)
+);
+
+CREATE TABLE dw.drug(
+    drug_id                             VARCHAR(128),
+    "route"                             VARCHAR(64),
+    dosage_form                         VARCHAR(128),
+    used_according_to_label             BOOLEAN,
+    off_label_use                       VARCHAR(128),
+    first_exposure_date                 VARCHAR(128),
+    last_exposure_date                  VARCHAR(128),
+    administered_by                     VARCHAR(128),
+    previous_exposure_to_drug           BOOLEAN,
+    previous_ae_to_drug                 BOOLEAN,
+    frequency_of_administration_value   VARCHAR(128),
+    frequency_of_administration_unit    VARCHAR(128),
+    dose_fraction                       VARCHAR(128),
+    dose_unit                           VARCHAR(128),
+    ae_abated_after_stopping_drug       VARCHAR(128),
+    ae_reappeared_after_resuming_drug   VARCHAR(128),
+    active_ingredient_name              VARCHAR(128)
+);
+
+CREATE TABLE dw.incident(
+    aer                                             VARCHAR(128),
+    receive_date                                    DATE,
+    animals_affected                                VARCHAR(128),
+    animals_treated                                 VARCHAR(128),
+    health_assessment_prior_to_exposure_condition   VARCHAR(128),
+    onset_date                                      VARCHAR(128),
+    time_between_exposure_and_onset                 VARCHAR(128),
+    treated_for_ae                                  BOOLEAN
+);
+
+CREATE TABLE dw.outcome(
+    outcome_id                          VARCHAR(128),
+    outcome_medical_status              VARCHAR(128),
+    outcome_number_of_animals_affected  VARCHAR(128)
+);
+
+
+CREATE TABLE dw.date(
+    date_dim_id              INT NOT NULL,
+    date_actual              DATE NOT NULL,
+    epoch                    BIGINT NOT NULL,
+    day_suffix               VARCHAR(4) NOT NULL,
+    day_name                 VARCHAR(9) NOT NULL,
+    day_of_week              INT NOT NULL,
+    day_of_week_iso          INT NOT NULL,
+    day_of_month             INT NOT NULL,
+    day_of_quarter           INT NOT NULL,
+    day_of_year              INT NOT NULL,
+    week_of_month            INT NOT NULL,
+    week_of_year             INT NOT NULL,
+    week_of_year_iso         CHAR(10) NOT NULL,
+    month_actual             INT NOT NULL,
+    month_name               VARCHAR(9) NOT NULL,
+    month_name_abbreviated   CHAR(3) NOT NULL,
+    quarter_actual           INT NOT NULL,
+    quarter_name             VARCHAR(9) NOT NULL,
+    year_actual              INT NOT NULL,
+    year_actual_iso          INT NOT NULL,
+    first_day_of_week        DATE NOT NULL,
+    last_day_of_week         DATE NOT NULL,
+    first_day_of_month       DATE NOT NULL,
+    last_day_of_month        DATE NOT NULL,
+    first_day_of_quarter     DATE NOT NULL,
+    last_day_of_quarter      DATE NOT NULL,
+    first_day_of_year        DATE NOT NULL,
+    last_day_of_year         DATE NOT NULL,
+    mmyyyy                   CHAR(6) NOT NULL,
+    mmddyyyy                 CHAR(10) NOT NULL,
+    weekend_indr             BOOLEAN NOT NULL
+);
+
+
+CREATE TABLE dw.fact(
+    reaction_id     VARCHAR(128),
+    animal_id       VARCHAR(128),
+    aer             VARCHAR(128),
+    drug_id         VARCHAR(128),
+    outcome_id      VARCHAR(128)
+);
---
+++ dw_etl.py
@@ -0,0 +1,423 @@
+import psycopg2
+
+# try:
+connection = psycopg2.connect(host='localhost',
+                                        database='vets_dw',
+                                        user='postgres',
+                                        password='password')
+
+####################################################################
+# Inserting data
+####################################################################
+
+print("============================================================")
+print("Starting to insert reaction....")
+print("============================================================")
+
+#Reaction
+reaction_select_Query = "select * from staging.FdaApiReaction"
+cursor = connection.cursor()
+cursor.execute(reaction_select_Query)
+# get all records
+records = cursor.fetchall()
+counter = 1 #For the id
+for row in records:
+    reaction_id = 'REACTION' + str(counter)
+    
+    if row[2] == []:
+        ved_version = 'unknown'
+    else:
+        ved_version = row[2]
+
+    if row[3] == []:
+        ved_term_code = 'unknown'
+    else:
+        ved_term_code = row[3]
+
+    if row[4] == []:
+        ved_term_name = 'unknown'
+    else:
+        ved_term_name = row[4]
+
+    animals_affected = row[5]
+    counter += 1
+
+    insert_reaction = """
+    INSERT INTO dw.reaction(
+        reaction_id,         
+        veddra_version,      
+        veddra_term_code,    
+        veddra_term_name,    
+        number_of_animals_affected
+        )
+    VALUES (%s, %s, %s, %s, %s)
+    """
+
+    cursor.execute(insert_reaction,[reaction_id, ved_version, ved_term_code, ved_term_name, animals_affected])
+
+    insert_facts1 = """
+
+        INSERT INTO dw.fact(
+            reaction_id
+        )
+        VALUES(%s)
+    """
+
+    cursor.execute(insert_facts1, [reaction_id])
+
+
+##################################################################
+
+print("============================================================")
+print("Starting to insert outcomes....")
+print("============================================================")
+
+#Outcome
+outcome_select_Query = "select * from staging.FdaApiOutcome"
+cursor = connection.cursor()
+cursor.execute(outcome_select_Query)
+# get all records
+records = cursor.fetchall()
+counter = 1 #For the id
+for row in records:
+    outcome_id = 'OUTCOME' + str(counter)
+    
+    if row[2] == []:
+        medical_status = 'unknown'
+    else:
+        medical_status = row[2]
+
+    number_of_animals_affected = row[3]
+
+    counter += 1
+
+    insert_outcome = """
+    INSERT INTO dw.outcome(
+        outcome_id,              
+        outcome_medical_status,              
+        outcome_number_of_animals_affected  
+        )
+    VALUES (%s, %s, %s)
+    """
+
+    cursor.execute(insert_outcome,[outcome_id, medical_status, number_of_animals_affected])
+
+    insert_facts2 = """
+
+        INSERT INTO dw.fact(
+            outcome_id
+        )
+        VALUES(%s)
+    """
+
+    cursor.execute(insert_facts2, [outcome_id])
+
+##################################################################
+
+print("============================================================")
+print("Starting to insert active ingredients....")
+print("============================================================")
+
+#ActiveIngredient
+active_select_Query = "select * from staging.FdaApiActiveIngredient"
+cursor = connection.cursor()
+cursor.execute(active_select_Query)
+# get all records
+records = cursor.fetchall()
+
+for row in records:
+    if row[3] == []:
+        name = 'unknown'
+    else:
+        name = row[3] 
+
+    dose_numerator = row[4]
+    dose_numerator_unit = row[5]
+    dose_denominator = row[6]
+    dose_denominator_unit = row[7]
+
+    if row[4] or row[6] == []:
+        dose_fraction = []
+    else:
+        dose_fraction = dose_numerator / dose_denominator
+    
+    if row[5] == []:
+        dose_unit = 'unknown'
+    else:
+        dose_unit = dose_numerator_unit
+
+    insert_drug1 = """
+    INSERT INTO dw.drug(  
+        dose_fraction,       
+        dose_unit,           
+        active_ingredient_name  
+        )                  
+    VALUES (%s, %s, %s)
+    """
+
+    cursor.execute(insert_drug1,[dose_fraction,dose_unit, name])
+
+##################################################################
+
+print("============================================================")
+print("Starting to insert drugs....")
+print("============================================================")
+
+#FdaApiDrug
+drug_select_Query = "select * from staging.FdaApiDrug"
+cursor = connection.cursor()
+cursor.execute(drug_select_Query)
+# get all records
+records = cursor.fetchall()
+for row in records:
+    drug_id = row[2]
+
+    if row[3] == []:
+        route = 'unknown'
+    else:
+        route = row[3]
+
+    if row[10] == []:
+        off_label_use = 'unknown'
+    else:
+        off_label_use = row[10]
+
+    if row[14] == []:
+        administered_by = 'unknown'
+    else:
+        administered_by = row[14]
+
+    if row[18] == []:
+        frequency_of_administration_value = 'unknown'
+    else:
+        frequency_of_administration_value = row[18]  
+
+    if row[19] == []:
+        frequency_of_administration_unit = 'unknown'
+    else:
+        frequency_of_administration_unit = row[19]
+
+    if row[24] == []:
+        ae_abated_after_stopping_drug = 'unknown'
+    else:
+        ae_abated_after_stopping_drug = row[24]
+
+    if row[25] == []:
+        ae_reappeared_after_resuming_drug = 'unknown'
+    else:
+        ae_reappeared_after_resuming_drug = row[25]  
+    
+    dosage_form = row[5]
+    used_according_to_label = row[9]  
+
+    first_exposure_date = row[12]
+    last_exposure_date = row[13] 
+    previous_exposure_to_drug = row[15]  
+    previous_ae_to_drug = row[16]
+
+    insert_drug2 = """
+    INSERT INTO dw.drug(  
+        drug_id,                              
+        route,                              
+        dosage_form,                          
+        used_according_to_label,             
+        off_label_use,                        
+        first_exposure_date,                  
+        last_exposure_date,                   
+        administered_by,                      
+        previous_exposure_to_drug,           
+        previous_ae_to_drug,                
+        frequency_of_administration_value,    
+        frequency_of_administration_unit,                          
+        ae_abated_after_stopping_drug,        
+        ae_reappeared_after_resuming_drug
+        )                            
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+
+    drugvalues = [drug_id,route, route, dosage_form,\
+        used_according_to_label,
+        off_label_use,
+        first_exposure_date,
+        last_exposure_date,
+        administered_by, 
+        previous_exposure_to_drug,           
+        previous_ae_to_drug,                
+        frequency_of_administration_value,    
+        frequency_of_administration_unit,                           
+        ae_abated_after_stopping_drug,        
+        ae_reappeared_after_resuming_drug,
+        ]
+
+    for item in drugvalues:
+        print(type(item))
+
+    cursor.execute(insert_drug2, drugvalues)
+
+    insert_facts3 = """
+
+        INSERT INTO dw.fact(
+            drug_id
+        )
+        VALUES(%s)
+    """
+
+    cursor.execute(insert_facts3, [drug_id])
+
+##################################################################
+
+print("============================================================")
+print("Starting to insert incidents....")
+print("============================================================")
+
+#FdaApiIncident
+incident_select_Query = "select * from staging.FdaApiIncident"
+cursor = connection.cursor()
+cursor.execute(incident_select_Query)
+# get all records
+records = cursor.fetchall()
+counter = 1
+for row in records:
+    aer = row[0]
+    original_receive_date = row[2]
+    number_of_animals_affected = row[3]
+
+    if row[4] == []:
+        primary_reporter = 'unknown'
+    else:
+        primary_reporter = row[4]
+    
+    number_of_animals_treated = row[5]
+    onset_date = row[6]
+    
+    if row[15] == []:
+        health_assessment_prior_to_exposure_assessed_by = 'unknown'
+    else:
+        health_assessment_prior_to_exposure_assessed_by = row[15]
+    
+    if row[16] == []:
+        species = 'unknown'
+    else:
+        species = row[16]
+    
+    if row[17] == []:
+        gender = 'unknown'
+    else:
+        gender = row[17]
+    
+    if row[18] == []:
+        female_animals_physiological_status = 'unknown'
+    else:
+        female_animals_physiological_status = row[18]
+    
+    if row[20] == []:
+        age_unit = 'unknown'
+    else:
+        age_unit = row[20]
+
+    age = row[19]
+    weight = row[22]
+    is_crossbred = row[25]
+
+    if row[26] == []:
+        breed_component = 'unknown'
+    else:
+        breed_component = row[26]
+
+    if row[29] == []:
+        reproductive_status = 'unknown'
+    else:
+        reproductive_status = row[29]
+
+    treated_for_ae = row[30]
+
+    if row[31] == []:
+        time_between_exposure_and_onset = 'unknown'
+    else:
+        time_between_exposure_and_onset = row[31]
+    
+    if row[32] == []:
+        health_assessment_prior_to_exposure_condition = 'unknown'
+    else:
+        health_assessment_prior_to_exposure_condition = row[32]
+
+
+    animal_id = 'ANIMALS'+str(counter)
+    counter += 1
+
+    insert_animals = """
+    INSERT INTO dw.animal(  
+        animal_id,                            
+        species, 	                         
+        gender, 	                             
+        female_animal_physiological_status, 	 
+        age,                                 
+        age_unit, 	                         
+        "weight_kg",    	                    
+        breed is_crossbred, 	                 
+        breed breed_component, 	             
+        reproductive_status 
+        )	                                
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+
+    cursor.execute(insert_animals,[\
+        animal_id,                            
+        species, 	                         
+        gender, 	                             
+        female_animals_physiological_status, 	 
+        age,                                 
+        age_unit, 	                         
+        weight,    	                    
+        is_crossbred, 	                 
+        breed_component, 	             
+        reproductive_status, 
+    ])
+
+
+    insert_incident = """INSERT INTO dw.incident(  
+        aer,                                  
+        receive_date,                        
+        animals_affected,                     
+        animals_treated,                      
+        health_assessment_prior_to_exposure_condition,  
+        onset_date,                                    
+        time_between_exposure_and_onset,      
+        treated_for_ae
+        )                     	                                
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """ 
+
+    cursor.execute(insert_incident,[\
+        aer,                                  
+        original_receive_date,                        
+        animals_affected,                     
+        number_of_animals_treated,                      
+        health_assessment_prior_to_exposure_condition,  
+        onset_date,                                    
+        time_between_exposure_and_onset,      
+        treated_for_ae      
+    ])
+
+    insert_facts4 = """
+
+        INSERT INTO dw.fact(
+            animal_id,
+            aer
+            )
+        VALUES(%s)
+    """
+
+    cursor.execute(insert_facts4, [animal_id,aer])
+
+
+##################################################################
+    
+# except psycopg2.connect.Error as e:
+#     print("Error reading data from PostgreSQL table", e)
+
+# finally:
+#     if connection.is_connected():
+#         connection.close()
+#         cursor.close()
+#         print("PostgreSQL connection is closed")
