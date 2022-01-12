f4bda0bc7520863f7facd2e21269e03c7461ac5e
Georgios Antoniadis <62813754+georgios-antoniadis@users.noreply.github.com>
2022-01-12 17:51:08 +0200

    Update dwTables.sql

--- sqls/dwTables.sql
+++ sqls/dwTables.sql
@@ -3,66 +3,72 @@ DROP TABLE if exists dw.date;
 DROP TABLE if exists dw.reaction;
 DROP TABLE if exists dw.animal;
 DROP TABLE if exists dw.incident;
+DROP TABLE if exists dw.active_ingredient;
 DROP TABLE if exists dw.outcome;
 DROP TABLE if exists dw.drug;
 DROP TABLE if exists dw.fact;
 
 CREATE TABLE dw.reaction(
-    reaction_id                 VARCHAR(128),
-    veddra_version              VARCHAR(128),
-    veddra_term_code            VARCHAR(128),
-    number_of_animals_affected  VARCHAR(128),
-    veddra_term_name            VARCHAR(128)
+    reaction_id                 VARCHAR(512),
+    veddra_version              VARCHAR(512),
+    veddra_term_code            VARCHAR(512),
+    number_of_animals_affected  VARCHAR(512),
+    veddra_term_name            VARCHAR(512)
 );
 
 CREATE TABLE dw.animal(
-    animal_id                           VARCHAR(128),
-    species 	                        VARCHAR(128),
-    gender 	                            VARCHAR(128),
-    female_animal_physiological_status 	VARCHAR(128),
+    animal_id                           VARCHAR(512),
+    species 	                        VARCHAR(512),
+    gender 	                            VARCHAR(512),
+    female_animal_physiological_status 	VARCHAR(512),
     age                                 DECIMAL(21,6),
-    age_unit 	                        VARCHAR(128),
+    age_unit 	                        VARCHAR(512),
     "weight_kg"    	                    DECIMAL(21,6),
-    is_crossbred 	                    VARCHAR(128),
-    breed_component 	                VARCHAR(128),
-    reproductive_status 	            VARCHAR(128)
+    is_crossbred 	                    VARCHAR(512),
+    breed_component 	                VARCHAR(512),
+    reproductive_status 	            VARCHAR(512)
+);
+
+CREATE TABLE dw.active_ingredient(
+    "ingredient_id" VARCHAR(64),
+    "drugID" VARCHAR(8),
+    "name" VARCHAR(256),
+    "dose_fraction" DECIMAL(21,6),
+    "dose_unit" VARCHAR(128)
 );
 
 CREATE TABLE dw.drug(
-    drug_id                             VARCHAR(128),
+    drug_id                             VARCHAR(512),
     "route"                             VARCHAR(64),
-    dosage_form                         VARCHAR(128),
+    dosage_form                         VARCHAR(512),
     used_according_to_label             BOOLEAN,
-    off_label_use                       VARCHAR(128),
-    first_exposure_date                 VARCHAR(128),
-    last_exposure_date                  VARCHAR(128),
-    administered_by                     VARCHAR(128),
+    off_label_use                       VARCHAR(512),
+    first_exposure_date                 VARCHAR(512),
+    last_exposure_date                  VARCHAR(512),
+    administered_by                     VARCHAR(512),
     previous_exposure_to_drug           BOOLEAN,
     previous_ae_to_drug                 BOOLEAN,
-    frequency_of_administration_value   VARCHAR(128),
-    frequency_of_administration_unit    VARCHAR(128),
-    dose_fraction                       NUMERIC,
-    dose_unit                           VARCHAR(128),
-    ae_abated_after_stopping_drug       VARCHAR(128),
-    ae_reappeared_after_resuming_drug   VARCHAR(128),
-    active_ingredient_name              VARCHAR(128)
+    frequency_of_administration_value   VARCHAR(512),
+    frequency_of_administration_unit    VARCHAR(512),
+    ae_abated_after_stopping_drug       VARCHAR(512),
+    ae_reappeared_after_resuming_drug   VARCHAR(512)
 );
 
 CREATE TABLE dw.incident(
-    aer                                             VARCHAR(128),
+    aer                                             VARCHAR(512),
     receive_date                                    DATE,
-    animals_affected                                VARCHAR(128),
-    animals_treated                                 VARCHAR(128),
-    health_assessment_prior_to_exposure_condition   VARCHAR(128),
-    onset_date                                      VARCHAR(128),
-    time_between_exposure_and_onset                 VARCHAR(128),
+    animals_affected                                VARCHAR(512),
+    animals_treated                                 VARCHAR(512),
+    health_assessment_prior_to_exposure_condition   VARCHAR(512),
+    onset_date                                      VARCHAR(512),
+    time_between_exposure_and_onset                 VARCHAR(512),
     treated_for_ae                                  BOOLEAN
 );
 
 CREATE TABLE dw.outcome(
-    outcome_id                          VARCHAR(128),
-    outcome_medical_status              VARCHAR(128),
-    outcome_number_of_animals_affected  VARCHAR(128)
+    outcome_id                          VARCHAR(512),
+    outcome_medical_status              VARCHAR(512),
+    outcome_number_of_animals_affected  VARCHAR(512)
 );
 
 
@@ -102,9 +108,9 @@ CREATE TABLE dw.date(
 
 
 CREATE TABLE dw.fact(
-    reaction_id     VARCHAR(128),
-    animal_id       VARCHAR(128),
-    aer             VARCHAR(128),
-    drug_id         VARCHAR(128),
-    outcome_id      VARCHAR(128)
+    reaction_id     VARCHAR(512),
+    animal_id       VARCHAR(512),
+    aer             VARCHAR(512),
+    drug_id         VARCHAR(512),
+    outcome_id      VARCHAR(512)
 );
